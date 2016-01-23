a6c0d233ccfe5ed6a0426ee2ae4bf4958150ff9b
Ernesto Jiménez <me@ernesto-jimenez.com>
2016-01-23 21:33:42 +0000

    Sort list of importer results

--- importers/dedup_list.go
+++ importers/dedup_list.go
@@ -1,6 +1,16 @@
 package importers
 
+import (
+	"sort"
+)
+
 type deduplist map[string]bool
+
+type sorted []string
+
+func (l sorted) Len() int           { return len(l) }
+func (l sorted) Swap(i, j int)      { l[i], l[j] = l[j], l[i] }
+func (l sorted) Less(i, j int) bool { return l[i] < l[j] }
 
 func (d deduplist) add(path string) {
 	d[path] = true
@@ -11,5 +21,6 @@ func (d deduplist) list() []string {
 	for path := range d {
 		list = append(list, path)
 	}
+	sort.Sort(sorted(list))
 	return list
 }
